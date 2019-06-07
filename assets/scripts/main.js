628a5eef388614bc1cb0a3ef7ec2945cebd56a30
Sarah <sarahjreive@gmail.com>
2019-06-07 18:25:05 +0000

    Created Folder structure, added HTML, CSS, and Script file. Comments contain tutorial notes

---
+++ assets/scripts/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() { //Runs after page is loaded
+
+  /* jQuery Action:
+   $('#selector').method1().method2().methodn();
+
+   Change CSS (Javascript Object):
+   $('#selector').css({key:value, fontWeight: 'bold'});
+
+   Change HTML:
+   $('#selector').html('change');
+
+   Event Listening:
+   $('#selector').on('event', function() {
+     Stuff that will happen when the user clicks
+     $('#selector').method()
+   })
+
+  //Searching by Attribute
+  $ ('.attribute').on('event', function() {
+    let variable = $(this).attr('attribute name');
+    $('#'+variable).method();
+  })
+
+*/
+});
