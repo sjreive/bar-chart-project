86ac2e53e903ba6fb3226c25ef658d7a47a55d60
Sarah <sarahjreive@gmail.com>
2019-06-20 00:28:58 +0000

    Modified drawBarChart to take an object, separate into object keys and values. Modified barWidth function so that xaxis labels are object keys.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
     let multiplier;
     for (let i = 0; i < array.length; i++) {
       multiplier = (array[i]/maxVal)*300;
-      document.getElementById("bar"+(i+1)).style.height = multiplier+"px"; //assigns height of bar based on input value
+      document.getElementById("bar"+(i+1)).style.height = multiplier+"px"; //assigns height of bar based on chartValues value
     }
   }
 
@@ -27,19 +27,19 @@ $(document).ready(function () {
     }
   }
 
-  function barWidth(array){
-    let length= array.length;
+  function barWidth(chartValues, valueLabels){
+    let length= chartValues.length;
     setBarWidth = 100/(length*1.5);
     setMarginWidth = setBarWidth / 4;
-    for (let i = 1; i <= array.length; i++) {
-     document.getElementById("bar" + i).style.width = setBarWidth+"%"; //sets width of bars based on number of input values
+    for (let i = 1; i <= chartValues.length; i++) {
+     document.getElementById("bar" + i).style.width = setBarWidth+"%"; //sets width of bars based on number of chartValues values
      document.getElementById("bar" + i).style.marginLeft = setMarginWidth+"%"; //sets the width of the margins on either side of the bars
      document.getElementById("bar" + i).style.marginRight = setMarginWidth+"%";
     // NTS figure out if there is a way to make this more efficient..
      document.getElementById("xlabel" + i).style.width = setBarWidth+"%";
      document.getElementById("xlabel" + i).style.marginLeft = setMarginWidth+"%"; //sets the width of the margins on either side of the bars
      document.getElementById("xlabel" + i).style.marginRight = setMarginWidth+"%";
-     document.getElementById("xlabel" + i).innerHTML = "LABEL";
+     document.getElementById("xlabel" + i).innerHTML = valueLabels[i-1];
     }
   }
 
@@ -95,24 +95,30 @@ $(document).ready(function () {
     $("<div/>",{id:"xaxis"}).appendTo("#xlabelspace");
   }
 
-  // Button allows user to input comma-separated data on click.
-  //$('#input').click (function()
-  //let input = prompt('Please input some data:');
+  // Button allows user to chartValues comma-separated data on click.
+  //$('#chartValues').click (function()
+  //let chartValues = prompt('Please chartValues some data:');
 
-  function drawBarChart(input){
-    input= input.split(",",10); //converts input string to an array
+  function drawBarChart(data){
+    let chartValues = Object.values(data); // array containing values
+    let valueLabels = Object.keys(data);
     generateChartSpace();
     generateYAxis();
-    generateYScale(input);
+    generateYScale(chartValues);
     generateYAxisTitle();
     generateXAxis();
-    generateXLabels(input);
+    generateXLabels(chartValues);
     generateXAxisTitle();
-    createBar(input); //calls createBar function to append a bar for each data element added
-    barHeight(input); // calls barHeight function
-    barWidth(input); // calls barWidth function
-    generateDataLabelSpace(input);
-    generateLabels(input);
+    createBar(chartValues); //calls createBar function to append a bar for each data element added
+    barHeight(chartValues); // calls barHeight function
+    barWidth(chartValues, valueLabels); // calls barWidth function
+    generateDataLabelSpace(chartValues);
+    generateLabels(chartValues);
   }
-drawBarChart("10,20,30,40");
+let object= {
+  apple: 28,
+  orange: 17,
+  pear: 54,
+}
+drawBarChart(object);
 });
